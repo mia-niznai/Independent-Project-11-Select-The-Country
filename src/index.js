780ac606705329654f5d51774939bd738d187322
mia-niznai <maria.popa.8735@gmail.com>
2023-01-29 18:38:41 +0200

    add index.js

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const countries = [
+    {
+      name: "Albania",
+      capital: "Tirana"
+    },
+    {
+      name: "Belgium",
+      capital: "Brussels"
+    },
+    {
+      name: "Bulgaria",
+      capital: "Sofia"
+    },
+    {
+      name: "Comoros",
+      capital: "Moroni"
+    },
+    {
+      name: "Denmark",
+      capital: "Copenhagen"
+    },
+    {
+      name: "Italy",
+      capital: "Rome"
+    },
+    {
+      name: "Liberia",
+      capital: "Monrovia"
+    },
+    {
+      name: "Madagascar",
+      capital: "Antananarivo"
+    },
+    {
+      name: "Nigeria",
+      capital: "Abuja"
+    },
+    {
+      name: "Singapore",
+      capital: "Singapore"
+    },
+    {
+      name: "Tajikistan",
+      capital: "Dushanbe"
+    },
+    {
+      name: "Zimbabwe",
+      capital: "Harare"
+    }
+  ];
