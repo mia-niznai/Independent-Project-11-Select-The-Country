46b071582acef3c15bc4a8e2b8953bf4f6cabf9a
mia-niznai <maria.popa.8735@gmail.com>
2023-01-29 18:43:51 +0200

    solutions for the tasks

--- src/index.js
+++ src/index.js
@@ -48,3 +48,28 @@ const countries = [
       capital: "Harare"
     }
   ];
+
+
+  //Your code comes below this line
+
+  //Solution
+
+  const input = document.querySelector("input");
+const textArea = document.querySelector("textarea");
+const select = document.querySelector("select");
+
+input.addEventListener("input", function(){
+  let filterValue = this.value.toLowerCase();
+  let criteria = select.value;
+  let filterCountries = countries.filter(function(country){
+    return country[criteria].toLowerCase().includes(filterValue);
+    })
+  .map(function(country){
+    return country.name;
+  });
+  textArea.value = filterCountries.join("\n")
+});
+
+
+
+  //Your code comes above this line
