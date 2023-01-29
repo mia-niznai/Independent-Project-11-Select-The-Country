40412afe94246e5b093067ebfe71ff8aaf532cf5
mia-niznai <maria.popa.8735@gmail.com>
2023-01-29 18:44:23 +0200

    tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+// Task 1
+// Each time you change the content of the input box, filter the countries that are displayed in the textarea to the ones having their name matching the typed content. Having no content in the input field displays all the countries.
+// Ex: typing ria would show only Bulgaria,Liberia,Nigeria (they both end in ria)
+
+// Task 2
+// Extend the filtering to be done by either name or capital based on the value of the select element. Each time the value of the select box is changed the displayed countries from the textarea are re-filtered by the criteria and the typed content from the input.
+
+// Task 3
+// Extend the filtering to be case insensitive (typing Alb produces the same result as typing alb).
+
+
 const countries = [
     {
       name: "Albania",
